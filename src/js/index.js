ea04be98182c74a78cc660ea51fc924a0a3171ef
Ben Lee <leeohsunshine@gmail.com>
2016-04-15 21:45:33 -0400

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 // Javascript Entry Point
+{/*<Router>
+  <Route path='/login' component={LogIn}/>
+  <Route path='/register' component={Register}/>
+  <Route path='/' component={Dashboard}>
+    <IndexRoute component={StuffInDasboard}/>
+  </Route>
+</Router>*/}

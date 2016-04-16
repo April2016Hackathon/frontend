947c6b1aaeb6475795691dc1c5605e3072fbb130
Heather Conley <hconley6695@gmail.com>
2016-04-16 11:11:21 -0400

    added currentuserview and submission

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,14 @@ import { Route, Router, hashHistory, IndexRoute } from 'react-router';
 import LogIn from './login';
 import Register from './register';
 import Dashboard from './dashboard';
+import Submission from './submission';
+import CurrentUserView from './currentUserView';
 
 render ((
 	<Router history={hashHistory}>
     <Route path='/login' component={LogIn}/>
     <Route path='/register' component={Register}/>
-    <Route path='/' component={Dashboard}>
+    <Route path='/' component={CurrentUserView}>
       {/*<IndexRoute component={StuffInDasboard}/>*/}
     </Route>
 	</Router>
--- src/js/submission.js
+++ src/js/submission.js
@@ -4,28 +4,45 @@ import { ajax } from 'jquery';
 import { hashHistory } from 'react-router';
 
 export default class Submission extends Component {
+    // constructor () {
+    //     super ();
+    //     this.state = { status: null} 
+    // }
 
 	dataHandler(moodstatus){
-      ajax({
-        url: 'http://',
-        type: 'POST',
-        cached: false,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      }).then(hashHistory.push('/'))
+        console.log(moodstatus);
+
+      // ajax({
+      //   url: 'http://',
+      //   type: 'POST',
+      //   cached: false,
+      //   dataType: 'json',
+      //   processData: false,
+      //   contentType: false
+      // }).then(hashHistory.push('/'))
     }
 
     render () {
+        return (
 
     	<div className="submission">
     		<h1>My Moods</h1>
-    		<SSF dataHandler={::this.dataHandler}>
-    			<button>Mild</button>
-    			<button>Wild</button>
-    			<imput type="text" name="status" placeholder="status"/>
+
+            <SSF onData={::this.dataHandler}> 
+              <label>MILD 			
+                <input type="radio" name="status" value={true} />
+              </label>    
+              <label>WILD
+                <input type="radio" name="status" value={false} /> 
+              </label> 
+              <label>
+                <input type="text" name="description" placeholder="description" />
+              </label>          
+                <button>Submit</button>
     		</SSF>
     	</div>
-    }
+   ) }
 
 }
+                // <button  type="button" onClick={() => this.state.status=false}>Mild</button>
+                // <button type="button" onClick={() =>this.state.status=true}>Wild</button>
